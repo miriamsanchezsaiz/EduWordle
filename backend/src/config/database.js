2ae0cfd91ad9472d2c59831fd78cf9df7c631d6c
Miriam <miriam.sanchezs@alumnos.upm.es>
2025-07-04 14:03:44 +0200

    revert "production"
    This reverts commit a6aa6a5ff92a145c35ed00dbc5bf6e00dcfa42b4.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -30,18 +30,15 @@ const config = {
     },  
     logging: false
    }
-   ,
-   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    dialectOptions: {
-      charset: 'utf8mb4'
-    },
-    logging: console.log,
-     }
+  // ,
+  // production: {
+  //   // username: process.env.DB_USER,
+  //   // password: process.env.DB_PASSWORD,
+  //   // database: process.env.DB_NAME,
+  //   // host: process.env.DB_HOST,
+  //   // dialect: 'mysql',
+  //   // logging: false
+  // }
 }
 
 const dbConfig = config[env];
---
+++ backend/src/utils/generateFrontendConfig.js
@@ -0,0 +1,30 @@
+// backend/src/utils/generate-frontend-config.js
+require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') }); 
+
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT ;
+const API_BASE_URL_FOR_FRONTEND = process.env.APP_URL + '/frontend' || `http://localhost:${PORT}/frontend`;
+const API_BASE_URL_FOR_BACKEND = process.env.APP_URL + '/api' || `http://localhost:${PORT}/api`;
+const configContent = `
+//Aquí se define la URL base de la API para que el frontend pueda hacer peticiones a ella.
+//Este URL se importa añadiendo el script al html y se usa en el fetch seguido de la ruta 
+// de la API que se quiera llamar.
+// Poe ejemplo: fetch(\`\${API_BASE_URL}/auth/login\`)
+// Este archivo es generado automáticamente por el backend. ¡No editar manualmente!
+export const API_BASE_URL = "${API_BASE_URL_FOR_BACKEND}";
+export const FRONT_URL = "${API_BASE_URL_FOR_FRONTEND}";
+
+
+//ATENCIÓN: hay que cambiar los href y los src de los html tal que:
+// href="assets/css/login.css" -> href="/assets/css/login.css"
+// src="assets/js/login.js" -> src="/assets/js/login.js"
+
+`;
+
+const outputPath = path.join(__dirname, '..', '..', '..', 'frontend', 'assets', 'js', 'apiConfig.js'); 
+
+fs.writeFileSync(outputPath, configContent.trim());
+
+console.log(`[Backend Script] Generated API configuration file for frontend: ${outputPath}`);
